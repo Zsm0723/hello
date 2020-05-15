8df6f8d5be4ddd7b0c2938d1de6dc9274439933a
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2020-05-15 17:10:04 +0300

    .......... [DEV-1553] rewrote LLD page test to new framework

--- frontends/php/tests/selenium/testPageLowLevelDiscovery.php
+++ frontends/php/tests/selenium/testPageLowLevelDiscovery.php
@@ -22,88 +22,88 @@ require_once dirname(__FILE__).'/../include/CWebTest.php';
 
 class testPageLowLevelDiscovery extends CWebTest {
 
-    const HOST_ID = 90001;
-    private $buttons_name = ['Disable', 'Enable', 'Check now', 'Delete'];
+	const HOST_ID = 90001;
+	private $buttons_name = ['Disable', 'Enable', 'Check now', 'Delete'];
 
-    public function testPageLowLevelDiscovery_CheckPageLayout() {
-        $this->page->login()->open('host_discovery.php?&hostid='.self::HOST_ID);
+	public function testPageLowLevelDiscovery_CheckPageLayout() {
+		$this->page->login()->open('host_discovery.php?&hostid='.self::HOST_ID);
 
-        // Checking Title name.
-        $this->assertPageTitle('Configuration of discovery rules');
+		// Checking Title name.
+		$this->assertPageTitle('Configuration of discovery rules');
 
-        // Checking Header name, that we are in a right place (sure we are, because we wrote already link to this host).
-        $page_title_name = $this->query('xpath://h1[@id="page-title-general"]')->one()->getText();
-        $this->assertEquals('Discovery rules', $page_title_name);
+		// Checking Header name, that we are in a right place (sure we are, because we wrote already link to this host).
+		$page_title_name = $this->query('xpath://h1[@id="page-title-general"]')->one()->getText();
+		$this->assertEquals('Discovery rules', $page_title_name);
 
-        // Now, check that there is realy 3 rows displayed.
-        $displayed_discovery = $this->query('xpath://div[@class="table-stats"]')->one()->getText();
-        $this->assertEquals('Displaying 3 of 3 found', $displayed_discovery);
+		// Now, check that there is realy 3 rows displayed.
+		$displayed_discovery = $this->query('xpath://div[@class="table-stats"]')->one()->getText();
+		$this->assertEquals('Displaying 3 of 3 found', $displayed_discovery);
 
-        // Here we go, with column value name check.
-        $table_headers = ['Items', 'Triggers', 'Graphs', 'Hosts', 'Info', 'Name', 'Key', 'Interval', 'Type', 'Status'];
-        foreach ($table_headers as $header) {
-            $this->assertTrue($this->query('xpath://tr//*[contains(text(),"'.$header.'")]')->one()->isPresent());
-        }
+		// Here we go, with column value name check.
+		$table_headers = ['Items', 'Triggers', 'Graphs', 'Hosts', 'Info', 'Name', 'Key', 'Interval', 'Type', 'Status'];
+		foreach ($table_headers as $header) {
+			$this->assertTrue($this->query('xpath://tr//*[contains(text(),"'.$header.'")]')->one()->isPresent());
+		}
 
-        // And now let's check that all buttons exists.
-        foreach ($this->buttons_name as $button) {
-            $this->assertTrue($this->query('xpath://button[text()="'.$button.'"]')->one()->isPresent());
-        }
-    }
+		// And now let's check that all buttons exists.
+		foreach ($this->buttons_name as $button) {
+			$this->assertTrue($this->query('xpath://button[text()="'.$button.'"]')->one()->isPresent());
+		}
+	}
 
-    public function testPageLowLevelDiscovery_CheckEnableSingle() {
-        $this->page->login()->open('host_discovery.php?&hostid='.self::HOST_ID);
+	public function testPageLowLevelDiscovery_CheckEnableSingle() {
+		$this->page->login()->open('host_discovery.php?&hostid='.self::HOST_ID);
 
-        // Get table element.
-        $table = $this->query('class:list-table')->asTable()->one();
+		// Get table element.
+		$table = $this->query('class:list-table')->asTable()->one();
 
-        // Find row by column value.
-        $row = $table->findRow('Name', 'Discovery rule 2')->select();
+		// Find row by column value.
+		$row = $table->findRow('Name', 'Discovery rule 2')->select();
 
-        // In this wonderful method, we press enabled/Disabled link and checking messages.
-        $actions = ['Enabled', 'Disabled'];
-        foreach ($actions as $action) {
-            $row->query('link:'.$action)->one()->click();
-            if ($action=='Enabled') {
-                $this->assertEquals('Discovery rule disabled', CMessageElement::find()->one()->getTitle());
-            }
-            elseif ($action=='Disabled'){
-                $this->assertEquals('Discovery rule enabled', CMessageElement::find()->one()->getTitle());
-            }
-        }
-    }
+		// In this wonderful method, we press enabled/Disabled link and checking messages.
+		$actions = ['Enabled', 'Disabled'];
+		foreach ($actions as $action) {
+			$row->query('link:'.$action)->one()->click();
+			if ($action=='Enabled') {
+				$this->assertEquals('Discovery rule disabled', CMessageElement::find()->one()->getTitle());
+			}
+			elseif ($action=='Disabled'){
+				$this->assertEquals('Discovery rule enabled', CMessageElement::find()->one()->getTitle());
+			}
+		}
+	}
 
-    public function testPageLowLevelDiscovery_CheckButtonsAll() {
-        $this->page->login()->open('host_discovery.php?&hostid='.self::HOST_ID);
+	public function testPageLowLevelDiscovery_CheckButtonsAll() {
+		$this->page->login()->open('host_discovery.php?&hostid='.self::HOST_ID);
 
-        // Here we press all available buttons and checking success message (if we have it).
-        foreach ($this->buttons_name as $button) {
-            $this->query('id:all_items')->asCheckbox()->one()->check();
-            $this->query('button:'.$button)->one()->click();
-            if ($button=='Disable') {
-                $this->page->acceptAlert();
-                $this->assertEquals('Discovery rules disabled', CMessageElement::find()->one()->getTitle());
-            }
-            elseif ($button=='Enable') {
-                $this->page->acceptAlert();
-                $this->assertEquals('Discovery rules enabled', CMessageElement::find()->one()->getTitle());
-            }
-            elseif ($button=='Check now') {
-                $this->assertEquals('Request sent successfully', CMessageElement::find()->one()->getTitle());
-            }
-            elseif ($button=='Delete') {
-                $this->assertEquals('Delete selected discovery rules?', $this->page->getAlertText());
-                $this->page->dismissAlert();
-            }
-        }
-    }
+		// Here we press all available buttons and checking success message (if we have it).
+		foreach ($this->buttons_name as $button) {
+			$this->query('id:all_items')->asCheckbox()->one()->check();
+			$this->query('button:'.$button)->one()->click();
+			if ($button=='Disable') {
+				$this->page->acceptAlert();
+				$this->assertEquals('Discovery rules disabled', CMessageElement::find()->one()->getTitle());
+			}
+			elseif ($button=='Enable') {
+				$this->page->acceptAlert();
+				$this->assertEquals('Discovery rules enabled', CMessageElement::find()->one()->getTitle());
+			}
+			elseif ($button=='Check now') {
+				$this->assertEquals('Request sent successfully', CMessageElement::find()->one()->getTitle());
+			}
+			elseif ($button=='Delete') {
+				$this->assertEquals('Delete selected discovery rules?', $this->page->getAlertText());
+				$this->page->dismissAlert();
+			}
+		}
+	}
 
-    public function testPageLowLevelDiscovery_CheckButtonsSingle() {
-        // Simply press "Check now" button. No rocket science
-        $this->page->login()->open('host_discovery.php?&hostid='.self::HOST_ID);
-        $table = $this->query('class:list-table')->asTable()->one();
-        $row = $table->findRow('Name', 'Discovery rule 3')->select();
-        $this->query('button:Check now')->one()->click();
-        $this->assertEquals('Request sent successfully', CMessageElement::find()->one()->getTitle());
-    }
+	public function testPageLowLevelDiscovery_CheckButtonsSingle() {
+		// Simply press "Check now" button. No rocket science
+		$this->page->login()->open('host_discovery.php?&hostid='.self::HOST_ID);
+		$table = $this->query('class:list-table')->asTable()->one();
+		$row = $table->findRow('Name', 'Discovery rule 3')->select();
+		$this->query('button:Check now')->one()->click();
+		$this->assertEquals('Request sent successfully', CMessageElement::find()->one()->getTitle());
+	}
 }
